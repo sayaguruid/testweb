fff06bcce5a33d58d3c2d29586745aad71137574
sayaguruid <teacheurs@gmail.com>
2025-12-01 09:15:42 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+// Ganti URL ini dengan URL Web App Google Apps Script Anda
+const API_URL = "https://script.google.com/macros/s/AKfycbxVx1qwdTkpBPPFxgwu8c8tSuzphITDrSK7hmOQ3TZu1EpR1ASE6T6nZtUiIzZ4aftc/exec";
